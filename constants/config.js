2609bde7b3d7a6dded569256150807275b8fc657
mohitmeena2107 <manumeherofficial@gmail.com>
2025-06-06 16:50:16 +0530

    backend ready

--- constants/config.js
+++ constants/config.js
@@ -3,3 +3,7 @@ const corsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true, // only include this if you are using cookies or Authorization headers
 };
+
+const CHITTER_TOKEN = "561651";
+
+export { corsOptions, CHITTER_TOKEN };
